c6ca8839d438af85c866a2b1daf4c888fcbdf6a6
Derin Gbadebo <daireenonline@yahoo.com>
2019-04-07 04:14:53 +0100

    Matching on package.

--- src/com/derinplayground/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/derinplayground/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyDemoLoggingAspect {
 //	Matches with method that matches exact object and any otber list of methods. (From zero to infinity)
-	@Before("execution(* add*(com.derinplayground.aopdemo.Account, ..))")
+	@Before("execution(* com.derinplayground.aopdemo.dao.*.*(com.derinplayground.aopdemo.Account, ..))")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n======> Executing the before advice on addAccount");
 	}
